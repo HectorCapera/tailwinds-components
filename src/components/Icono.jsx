a8db407cd57ec05becf8b894cde0d26a69dded2b
Hector Capera <109114807+HectorCapera@users.noreply.github.com>
2024-01-29 08:20:47 -0500

    layout componets

---
+++ src/components/Icono.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const IconoTailwind = () => {
+  return (
+    <div class=" flex flex-col justify-center items-center">
+      <div class="text-xl font-light text-white text-center mt-10 mb-10">
+        Componentes, herramientas y recursos para tu web
+      </div>
+      <img src="/tailwindcss.svg" alt="tailwind Icon" class="w-22 h-20" />
+      <div class="text-5xl font-bold text-white text-center mt-10">
+        Desarrolla rápidamente sitios web modernos con Tailwind CSS
+      </div>
+    </div>
+  );
+};
+
+export default IconoTailwind;
+
+// Path: src/components/Icono.jsx
